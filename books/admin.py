09dd55eba97e4bda9c6c81a8fea54c34f052ec1d
Samandar <samandarshoyimov04@gmail.com>
2025-11-04 14:20:00 +0100

    books admin added

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Genre, Book
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'books_count']
+    search_fields = ['name']
+
+    def books_count(self, obj):
+        return obj.books.count()
+    books_count.short_description = 'Number of Books'
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author_name', 'genre', 'publication_year', 'is_premium', 'created_at']
+    list_filter = ['is_premium', 'genre', 'publication_year']
+    search_fields = ['title', 'author_name', 'description']
+    list_select_related = ['genre']
+    date_hierarchy = 'created_at'
+    readonly_fields = ['created_at']
+
