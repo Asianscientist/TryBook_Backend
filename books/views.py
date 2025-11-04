a4d127371d156355e20f649f3267eb3d50f8d7d5
Samandar <samandarshoyimov04@gmail.com>
2025-11-04 14:12:37 +0100

    genre viewset added

--- books/views.py
+++ books/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import GenreSerializer, BookListSerializer, BookSerializer
+from .models import Book, Genre
 
-# Create your views here.
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['name', 'id']
+
+    @action(detail=True, methods=['get'])
+    def books(self, request, pk=None):
+        genre = self.get_object()
+        books = genre.books.all()
+        serializer = BookListSerializer(books, many=True)
+        return Response(serializer.data)
+
+
